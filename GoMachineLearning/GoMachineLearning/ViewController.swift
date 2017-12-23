3499baf040a02733c5a171684b96d37cb5197259
Sanjay Kumaran <sanjaykumaran@Sanjays-MacBook-Pro.local>
2017-12-23 11:19:37 +0530

    First Upload

---
+++ GoMachineLearning/GoMachineLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoMachineLearning
+//
+//  Created by Sanjay Kumaran on 23/12/17.
+//  Copyright © 2017 Sanjay Kumaran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
