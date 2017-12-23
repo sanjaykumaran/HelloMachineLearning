48453451b6ff944b26d27572fc2696676f7f17a9
Sanjay Kumaran <sanjaykumaran@Sanjays-MacBook-Pro.local>
2017-12-23 11:26:00 +0530

    final upload of gender guessing

--- GoMachineLearning/GoMachineLearning/ViewController.swift
+++ GoMachineLearning/GoMachineLearning/ViewController.swift
@@ -8,13 +8,72 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameLabel: UITextField!
+    @IBOutlet weak var genderLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nameLabel.delegate = self
+        
     }
-
+    
+    // string="nina"
+    /*  "firstLetter1=n"    :1.0    */
+    /*  "firstLetter2=i"    :1.0    */
+    /*  "firstLetter3=n"    :1.0    */
+    /*  "lastLetter1=i"     :1.0    */
+    /*  "lastLetter2=n"     :1.0   */
+    /*  "lastLetter3=a"     :1.0    */
+    
+    func features (_ string:String) -> [String:Double] {
+        guard !string.isEmpty else {return [:]}
+        
+        let string = string.lowercased()
+        var keys = [String]()
+        
+        keys.append("firstLetter1=\(string.prefix(1))")
+        keys.append("firstLetter2=\(string.prefix(2))")
+        keys.append("firstLetter3=\(string.prefix(3))")
+        keys.append("lastLetter1=\(string.suffix(1))")
+        keys.append("lastLetter2=\(string.suffix(2))")
+        keys.append("lastLetter3=\(string.suffix(3))")
+        
+        return keys.reduce([String: Double]()) { (result, key) -> [String: Double] in
+            var result = result
+            result[key] = 1.0
+            
+            return result
+        }
+        
+    }
+    
+    func predictGenderFromName (_ name:String) -> String? {
+        let nameFeatures = features(name)
+        
+        let model = GenderByName()
+        
+        if let prediction = try? model.prediction(input: nameFeatures) {
+            if prediction.classLabel == "F" {
+                return "Female"
+            }else{
+                return "Male"
+            }
+        }
+        
+        return nil
+        
+    }
+    
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        genderLabel.text = predictGenderFromName(nameLabel.text!)
+        
+        return true
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
